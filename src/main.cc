56d2109dc4153b94651e1728917d4b2b0b7e1b6b
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2019-05-03 18:42:15 +0200

    Better present the animation data. Describe skin joint topology

--- src/main.cc
+++ src/main.cc
@@ -78,6 +78,74 @@ static void mouseButtonCallback(int button, int state, float x, float y) {
     retturn
 #endif
 
+static int find_mesh_with_node_in_children(const tinygltf::Model &model,
+                                           int root) {
+  const auto &root_node = model.nodes[root];
+  if (root_node.mesh >= 0) return root;
+
+  for (auto child : root_node.children) {
+    const auto result = find_mesh_with_node_in_children(model, child);
+    if (result > 0 && model.nodes[result].mesh >= 0) return result;
+  }
+
+  return -1;
+}
+
+static int find_main_mesh_node(const tinygltf::Model &model) {
+  const auto &node_list =
+      model.scenes[model.defaultScene >= 0 ? model.defaultScene : 0].nodes;
+
+  for (auto node : node_list) {
+    const auto mesh_node = find_mesh_with_node_in_children(model, node);
+    if (mesh_node >= 0) return mesh_node;
+  }
+
+  return -1;
+}
+
+static void describe_node_topology(const tinygltf::Model &model,
+                                   int node_index) {
+  std::string node_desc = "node [" + std::to_string(node_index) + "]";
+  if (ImGui::TreeNode(node_desc.c_str())) {
+    const auto &node = model.nodes[node_index];
+    if (!node.name.empty()) ImGui::Text("name [%s]", node.name.c_str());
+
+    for (auto child : node.children) {
+      describe_node_topology(model, child);
+    }
+
+    ImGui::TreePop();
+  }
+}
+static void model_info_window(const tinygltf::Model &model) {
+  // TODO also cache info found here
+  if (ImGui::Begin("Model information")) {
+    const auto main_node_index = find_main_mesh_node(model);
+    if (main_node_index < 0) {
+      ImGui::Text("Could not find a node with a mesh in your glTF file!");
+      return;
+    }
+
+    ImGui::Text("Main node with a mesh [%d]");
+    const auto &main_node = model.nodes[main_node_index];
+
+    if (main_node.skin >= 0) {
+      // mesh.weights
+      ImGui::Text("Main mesh uses skin [%d]", main_node.skin);
+      const auto &skin = model.skins[main_node.skin];
+      ImGui::Text("Skin [%d] skeleton root node [%d]", main_node.skin,
+                  skin.skeleton);
+      ImGui::Text("Skin joint count [%d]", skin.joints.size());
+      if (ImGui::CollapsingHeader("Skeleton topology"))
+        describe_node_topology(model, skin.skeleton);
+    }
+
+    // TODO check if the found node with a mesh has morph targets, and
+    // describe them in this window here
+  }
+  ImGui::End();
+}
+
 static void animation_window(const tinygltf::Model &model) {
   // TODO(syoyo): Cache values
 
@@ -100,61 +168,10 @@ static void animation_window(const tinygltf::Model &model) {
     animation_names.push_back(name);
   }
 
-  int idx = 0;
+  static int idx = 0;
   ImGuiCombo("animations", &idx, animation_names);
 
   const tinygltf::Animation &animation = model.animations[size_t(idx)];
-
-  //
-  // samplers
-  //
-  if (ImGui::TreeNode("samplers")) {
-    std::vector<std::string> sampler_names;
-
-    for (size_t i = 0; i < animation.samplers.size(); i++) {
-      std::string name = "sampler_" + std::to_string(i);
-      sampler_names.push_back(name);
-    }
-
-    if (sampler_names.empty()) {
-      ImGui::Text("??? no samplers in animation.");
-    } else {
-      static int sampler_idx = 0;
-      ImGuiCombo("samplers", &sampler_idx, sampler_names);
-
-      const tinygltf::AnimationSampler &sampler =
-          animation.samplers[sampler_idx];
-
-      ImGui::Text("input  [%d]", sampler.input);
-      ImGui::Text("output [%d]", sampler.output);
-
-      float min_v, max_v;
-      if (tinygltf::util::GetAnimationSamplerInputMinMax(sampler, model, &min_v,
-                                                         &max_v)) {
-        ImGui::Text("Keyframe range : %f, %f [secs]", min_v, max_v);
-
-        int count =
-            tinygltf::util::GetAnimationSamplerInputCount(sampler, model);
-        ImGui::Text("# of key frames : %d", count);
-
-        const tinygltf::Accessor &accessor = model.accessors[sampler.input];
-        for (int k = 0; k < count; k++) {
-          float value;
-          if (tinygltf::util::DecodeScalarAnimationValue(size_t(k), accessor,
-                                                         model, &value)) {
-            // TODO(syoyo): Use table.
-            ImGui::Text("%d : %f", k, value);
-          }
-        }
-
-      } else {
-        ImGui::TextColored(ImVec4(0.9f, 0.2f, 0.1f, 1.0f),
-                           "input accessor must have min/max property");
-      }
-    }
-
-    ImGui::TreePop();
-  }
 
   //
   // channels
@@ -182,51 +199,186 @@ static void animation_window(const tinygltf::Model &model) {
 
       const tinygltf::AnimationSampler &sampler =
           animation.samplers[channel.sampler];
+      ImGui::Text("interpolation mode [%s]", sampler.interpolation.c_str());
+
       const tinygltf::Accessor &accessor = model.accessors[sampler.output];
       int count =
           tinygltf::util::GetAnimationSamplerOutputCount(sampler, model);
 
       if (channel.target_path.compare("weights") == 0) {
+        ImGui::Columns(2);
+        ImGui::Separator();
+        ImGui::Text("Frame");
+        ImGui::NextColumn();
+        ImGui::Text("Weight Value");
+        ImGui::NextColumn();
+        ImGui::Separator();
         for (int k = 0; k < count; k++) {
           float value;
           if (tinygltf::util::DecodeScalarAnimationValue(size_t(k), accessor,
                                                          model, &value)) {
-            // TODO(syoyo): Use table.
-            ImGui::Text("%d : %f", k, value);
+            ImGui::Text("%d", k);
+            ImGui::NextColumn();
+            ImGui::Text("%f", value);
+            ImGui::NextColumn();
+            ImGui::Separator();
           }
         }
+        ImGui::Columns();
       } else if (channel.target_path.compare("translation") == 0) {
+        ImGui::Columns(4);
+        ImGui::Separator();
+        ImGui::Text("Frame");
+        ImGui::NextColumn();
+        ImGui::Text("X");
+        ImGui::NextColumn();
+        ImGui::Text("Y");
+        ImGui::NextColumn();
+        ImGui::Text("Z");
+        ImGui::NextColumn();
+        ImGui::Separator();
         for (int k = 0; k < count; k++) {
           float xyz[3];
           if (tinygltf::util::DecodeTranslationAnimationValue(
                   size_t(k), accessor, model, xyz)) {
-            // TODO(syoyo): Use table.
-            ImGui::Text("%d : (%f, %f, %f)", k, xyz[0], xyz[1], xyz[2]);
+            ImGui::Text("%d", k);
+            ImGui::NextColumn();
+            ImGui::Text("%f", xyz[0]);
+            ImGui::NextColumn();
+            ImGui::Text("%f", xyz[1]);
+            ImGui::NextColumn();
+            ImGui::Text("%f", xyz[2]);
+            ImGui::NextColumn();
+            ImGui::Separator();
           }
         }
+        ImGui::Columns();
       } else if (channel.target_path.compare("scale") == 0) {
+        ImGui::Columns(4);
+        ImGui::Separator();
+        ImGui::Text("Frame");
+        ImGui::NextColumn();
+        ImGui::Text("X");
+        ImGui::NextColumn();
+        ImGui::Text("Y");
+        ImGui::NextColumn();
+        ImGui::Text("Z");
+        ImGui::NextColumn();
+        ImGui::Separator();
         for (int k = 0; k < count; k++) {
           float xyz[3];
           if (tinygltf::util::DecodeScaleAnimationValue(size_t(k), accessor,
                                                         model, xyz)) {
-            // TODO(syoyo): Use table.
-            ImGui::Text("%d : (%f, %f, %f)", k, xyz[0], xyz[1], xyz[2]);
+            ImGui::Text("%d", k);
+            ImGui::NextColumn();
+            ImGui::Text("%f", xyz[0]);
+            ImGui::NextColumn();
+            ImGui::Text("%f", xyz[1]);
+            ImGui::NextColumn();
+            ImGui::Text("%f", xyz[2]);
+            ImGui::NextColumn();
+            ImGui::Separator();
           }
         }
+        ImGui::Columns();
       } else if (channel.target_path.compare("rotation") == 0) {
+        ImGui::Columns(5);
+        ImGui::Separator();
+        ImGui::Text("Frame");
+        ImGui::NextColumn();
+        ImGui::Text("X");
+        ImGui::NextColumn();
+        ImGui::Text("Y");
+        ImGui::NextColumn();
+        ImGui::Text("Z");
+        ImGui::NextColumn();
+        ImGui::Text("W");
+        ImGui::NextColumn();
+        ImGui::Separator();
         for (int k = 0; k < count; k++) {
           float xyzw[4];
           if (tinygltf::util::DecodeRotationAnimationValue(size_t(k), accessor,
                                                            model, xyzw)) {
-            // TODO(syoyo): Use table.
-            ImGui::Text("%d : (%f, %f, %f, %f)", k, xyzw[0], xyzw[1], xyzw[2],
-                        xyzw[3]);
+            ImGui::Text("%d", k);
+            ImGui::NextColumn();
+            ImGui::Text("%f", xyzw[0]);
+            ImGui::NextColumn();
+            ImGui::Text("%f", xyzw[1]);
+            ImGui::NextColumn();
+            ImGui::Text("%f", xyzw[2]);
+            ImGui::NextColumn();
+            ImGui::Text("%f", xyzw[3]);
+            ImGui::NextColumn();
+            ImGui::Separator();
           }
         }
+        ImGui::Columns();
       } else {
         ImGui::TextColored(ImVec4(0.8f, 0.2f, 0.1f, 1.0f),
                            "??? Unknown target path name [%s]",
                            channel.target_path.c_str());
+      }
+    }
+
+    ImGui::TreePop();
+  }
+
+  //
+  // samplers
+  //
+  if (ImGui::TreeNode("samplers")) {
+    std::vector<std::string> sampler_names;
+
+    for (size_t i = 0; i < animation.samplers.size(); i++) {
+      std::string name = "sampler_" + std::to_string(i);
+      sampler_names.push_back(name);
+    }
+
+    if (sampler_names.empty()) {
+      ImGui::Text("??? no samplers in animation.");
+    } else {
+      static int sampler_idx = 0;
+      ImGuiCombo("samplers", &sampler_idx, sampler_names);
+
+      const tinygltf::AnimationSampler &sampler =
+          animation.samplers[sampler_idx];
+
+      ImGui::Text("input  [%d]", sampler.input);
+      ImGui::Text("output [%d]", sampler.output);
+      ImGui::Text("Interpolation method [%s]", sampler.interpolation.c_str());
+
+      float min_v, max_v;
+      if (tinygltf::util::GetAnimationSamplerInputMinMax(sampler, model, &min_v,
+                                                         &max_v)) {
+        ImGui::Text("Keyframe range : %f, %f [secs]", min_v, max_v);
+
+        int count =
+            tinygltf::util::GetAnimationSamplerInputCount(sampler, model);
+        ImGui::Text("# of key frames : %d", count);
+
+        const tinygltf::Accessor &accessor = model.accessors[sampler.input];
+        ImGui::Columns(2);
+        ImGui::Text("Frame Number");
+        ImGui::NextColumn();
+        ImGui::Text("Timestamp");
+        ImGui::NextColumn();
+        ImGui::Separator();
+        for (int k = 0; k < count; k++) {
+          float value;
+          if (tinygltf::util::DecodeScalarAnimationValue(size_t(k), accessor,
+                                                         model, &value)) {
+            ImGui::Text("%d", k);
+            ImGui::NextColumn();
+            ImGui::Text("%f", value);
+            ImGui::NextColumn();
+            ImGui::Separator();
+          }
+        }
+        ImGui::Columns();
+
+      } else {
+        ImGui::TextColored(ImVec4(0.9f, 0.2f, 0.1f, 1.0f),
+                           "input accessor must have min/max property");
       }
     }
 
@@ -390,6 +542,7 @@ int main(int argc, char **argv) {
       ImGui::End();
     }
 
+    model_info_window(model);
     animation_window(model);
 
     {
