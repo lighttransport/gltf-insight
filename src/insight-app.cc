ae199d03ccd09365686103633420dea4f956988c
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2019-06-30 04:20:14 +0200

    inverse(parent_world) * bone_world = pose. TODO apply delta to this.

--- src/insight-app.cc
+++ src/insight-app.cc
@@ -1422,61 +1422,69 @@ void app::run_3D_gizmo(gltf_node* active_bone) {
 
   ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
 
+  const auto unmanipulated_matrix = glm::make_mat4(manipulated_matrix);
+
   if (show_gizmo)
     ImGuizmo::Manipulate(
         glm::value_ptr(view_matrix), glm::value_ptr(projection_matrix),
         mCurrentGizmoOperation, mCurrentGizmoMode, manipulated_matrix,
         static_cast<float*>(glm::value_ptr(delta_matrix)));
 
-  // if (current_mode == 1) {
-  // todo modify active_bone->pose to match the new bone global matrix
+  if (current_mode == 1) {
+    gltf_node::animation_state& pose = active_bone->pose;
 
-  // gltf_node::animation_state& pose = active_bone->pose;
-  //// we actually don't need the skew and perpective ones, but GLM API does
-  // static glm::vec3 position, scale, skew;
-  // static glm::quat rotation;
-  // static glm::vec4 perspective;
+    // we actually don't need the skew and perpective ones, but GLM API does
+    static glm::vec3 position(0.f), scale(1.f), skew(1.f);
+    static glm::quat rotation(1.f, 0.f, 0.f, 0.f);
+    static glm::vec4 perspective(1.f);
+    (void)skew;
+    (void)perspective;
 
-  // glm::mat4 parent_joint_matrix(1.f), parent_bind_matrix(1.f);
+    // pose is relative to the parent
+    glm::mat4 parent_bone_world_xform(1.f);
 
-  // if (active_bone->parent) try {
-  //    const auto joint_index =
-  //        size_t(a_mesh->joint_inverse_bind_matrix_map.at(
-  //            active_bone->parent->gltf_node_index));
+    // check if there's a parent bone
+    if (active_bone->parent) {
+      parent_bone_world_xform = active_bone->parent->world_xform;
+    }
 
-  //    parent_bind_matrix =
-  //        glm::inverse(a_mesh->inverse_bind_matrices[joint_index]);
-  //    parent_joint_matrix = a_mesh->joint_matrices[joint_index];
-  //  } catch (const std::exception& e) {
-  //    std::cerr << e.what() << "\n";
-  //  }
+    // The pose is expressed in the world referential, but skeleton and
+    // manipulator are in the mesh's referential. The skeleton and mesh don't
+    // necessarly line-up
 
-  // Get the referential
-  // const auto referential =
-  //    mesh_node->local_xform * parent_joint_matrix * parent_bind_matrix;
+    glm::mat4 transformed_world_xform = active_bone->world_xform;
+    // glm::inverse(mesh_node->world_xform) *
+    // delta_matrix;
 
-  // Decompose that matrix into position rotation sacale
-  // glm::decompose(glm::inverse(referential) * bone_world_xform, scale,
-  //               rotation, position, skew, perspective);
+    // Decompose that matrix into position rotation scale
 
-  // ImGui::Text("Decomposed pose :");
-  // ImGui::Text("t = %f %f %f", position.x, position.y, position.z);
-  // ImGui::Text("r = %f %f %f %f", rotation.w, rotation.x, rotation.y,
-  //            rotation.z);
-  // ImGui::Text("s = %f %f %f", scale.x, scale.y, scale.z);
+    // This matrix once decomposed is the same transform as the currently held
+    // pose
+    auto currently_posed =
+        glm::inverse(parent_bone_world_xform) * active_bone->world_xform;
 
-  // ImGui::Text("Current pose:");
-  // ImGui::Text("t = %f %f %f", pose.translation.x, pose.translation.y,
-  //            pose.translation.z);
-  // ImGui::Text("r = %f %f %f %f", pose.rotation.w, pose.rotation.x,
-  //            pose.rotation.y, pose.rotation.z);
-  // ImGui::Text("s = %f %f %f", pose.scale.x, pose.scale.y, pose.scale.z);
+    // TODO apply the gizmo delta matrix to the above without making everything
+    // explode
 
-  // apply to pose
-  // pose.translation = position;
-  // pose.rotation = rotation;
-  // pose.scale = scale;
-  //}
+    ImGui::Text("Decomposed result :");
+    ImGui::Text("t = %f %f %f", position.x, position.y, position.z);
+    ImGui::Text("r = %f %f %f %f", rotation.w, rotation.x, rotation.y,
+                rotation.z);
+    ImGui::Text("s = %f %f %f", scale.x, scale.y, scale.z);
+
+    ImGui::Text("Current pose:");
+    ImGui::Text("t = %f %f %f", pose.translation.x, pose.translation.y,
+                pose.translation.z);
+    ImGui::Text("r = %f %f %f %f", pose.rotation.w, pose.rotation.x,
+                pose.rotation.y, pose.rotation.z);
+    ImGui::Text("s = %f %f %f", pose.scale.x, pose.scale.y, pose.scale.z);
+
+    if (delta_matrix != glm::mat4(1.f)) {
+      pose.translation = position;
+      pose.rotation = rotation;
+      pose.scale = scale;
+    }
+  }
 }
 
 void app::fill_sequencer() {
