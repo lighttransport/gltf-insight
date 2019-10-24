702ecb1d9f273cf601593befe3e6b077bc01f919
Syoyo Fujita <syoyo@lighttransport.com>
2019-10-24 19:14:03 +0900

    Use nativefiledialog on Windows.

--- vcsetup.bat
+++ vcsetup.bat
@@ -1,4 +1,6 @@
 rmdir /q /s build
 mkdir build
 
-cmake.exe -G "Visual Studio 15 2017 Win64" -Bbuild -H.
+cd build
+
+cmake.exe -G "Visual Studio 15 2017" -A x64 -DGLTF_INSIGHT_USE_NATIVEFILEDIALOG=On ..
